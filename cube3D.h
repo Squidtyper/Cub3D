9024faab5924aee4f499eefc6eee9bdf5b1dc829
greedymelon <74475072+greedymelon@users.noreply.github.com>
2023-03-08 14:57:02 +0100

    new structure

--- cube3D.h
+++ cube3D.h
@@ -3,7 +3,8 @@
 # include "MLX42/include/MLX42/MLX42.h"
 # include "libft/libft.h"
 # define PI 3.1415926535
-
+# define P2 PI/2
+# define P3 3*PI/2
 
 extern uint32_t	mappa [];
 
@@ -16,13 +17,22 @@ typedef struct s_rays
 	int map_y;
 	int map_pos;
 	int dof;
-	int i_ray;
 	double x;
 	double y;
 	double angle;
+	double a_tan;
 	double x_offset;
 	double y_offset;
 } t_rays;
+
+
+typedef struct s_ray_end_point
+{
+	double hor_x;
+	double hor_y;
+	double ver_x;
+	double ver_y;
+} t_ray_end_point;
 
 typedef struct s_player
 {
@@ -43,7 +53,7 @@ typedef struct s_image_mlx
 }	t_image_mlx;
 
 void	draw_line(t_player *player);
-void	draw_lineray(t_player *player, t_rays ray);
+void	draw_lineray(t_player *player, double end_x, double end_y);
 void	draw_rays_2D (t_player *player);
 
 void	key_w(t_image_mlx *img);
