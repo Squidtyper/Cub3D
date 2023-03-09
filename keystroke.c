efef9898eaf674e469aebc359ce289efa197d711
Debora Monfrini <dmonfrin@f1r2s6.codam.nl>
2023-03-09 17:19:41 +0100

    missing wall collision whit a and d

--- keystroke.c
+++ keystroke.c
@@ -12,19 +12,23 @@ void	key_w(t_image_mlx *img, t_wall_coll *set)
 void	key_s(t_image_mlx *img, t_wall_coll *set)
 {
 	if(mappa[set->ipy*map_x +set->ipx_sub_xo] == 0)
-		img->player.x -= img->player.delta_y;
-	if(mappa[set->ipy_sub_yo*map_x +set->ipx] == 0)
 		img->player.x -= img->player.delta_x;
+	if(mappa[set->ipy_sub_yo*map_x +set->ipx] == 0)
+		img->player.y -= img->player.delta_y;
 }
 
 void	key_a(t_image_mlx *img, t_wall_coll *set)
 {
 	img->player.y -= img->player.delta_x;
 	img->player.x += img->player.delta_y;
+	if(set)
+		return ;
 }
 
 void	key_d(t_image_mlx *img, t_wall_coll *set)
 {
 	img->player.y += img->player.delta_x;
 	img->player.x -= img->player.delta_y;
+	if(set)
+	return ;
 }
