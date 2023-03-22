b461aa9d4a79decbde0989a9eef733569b88324b
lizhang <lizhang@student.codam.nl>
2023-03-22 18:01:57 +0100

    reorganized the headers

--- include/cube3D.h
+++ include/cube3D.h
@@ -1,9 +1,22 @@
 #ifndef CUBE3D_H
 # define CUBE3D_H
-# include "parsing.h"
 # define HEIGHT_WIDTH 512
+
+typedef struct s_input
+{
+	char			**map_points;
+	unsigned int	map_width;
+	unsigned int	map_height;
+	long long		c_color;
+	long long		f_color;
+	t_list			*textures;
+	float			a_player;
+	int				player_x;
+	int				player_y;
+}	t_input;
 
 void		execution(t_input *input);
 t_input 	*parse(int ac, char **av);
+void		clean_input(t_input *input);
 
 #endif
--- include/parsing.h
+++ include/parsing.h
@@ -1,22 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   parsing.h                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: lizhang <lizhang@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/22 14:52:22 by lizhang           #+#    #+#             */
-/*   Updated: 2023/03/22 17:14:37 by lizhang          ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   parsing.h                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lizhang <lizhang@student.42.fr>              +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/03/22 14:52:22 by lizhang       #+#    #+#                 */
+/*   Updated: 2023/03/22 18:01:26 by lizhang       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSING_H
-#define PARSING_H
-#include <readline/readline.h>
-#include <fcntl.h>
-#include <stdio.h>
+# define PARSING_H
+# include <readline/readline.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include "cube3D.h"
 # include "../MLX42/include/MLX42/MLX42.h"
-#include "../libft/libft.h"
+# include "../libft/libft.h"
 
 typedef struct s_f_con
 {
@@ -24,21 +25,13 @@ typedef struct s_f_con
 	char			**lines;
 }	t_f_con;
 
-typedef struct s_input
+typedef struct s_check
 {
-	char			**map_points;
-	unsigned int	map_width;
-	unsigned int	map_height;
-	long long		c_color;
+	t_input			*input;
 	bool			c_found;
-	long long		f_color;	
 	bool			f_found;
-	t_list			*textures;
-	float			a_player;
-	int				player_x;
-	int				player_y;
 	bool			p_found;
-}	t_input;
+}	t_check;
 
 void			ac_error(int ac);
 void			mallocerr(void);
@@ -48,18 +41,18 @@ char			*join_free(char *str1, char *str2);
 char			**ft_space_split(char *str);
 int				open_file(char *name);
 void    		ac_error(int ac);
-void			parse_color(char **words, t_input *input);
+void			parse_color(char **words, t_check *check);
 void			color_double(bool testvalue);
-void			find_color(t_input *input, t_f_con *f_con);
+void			find_color(t_check *check, t_f_con *f_con);
 int				only_digits(char *str);
-void			find_texture(t_input *input, t_f_con *f_con);
-void			get_player(t_input *input);
+void			find_texture(t_check *check, t_f_con *f_con);
+void			get_player(t_check *check);
 int				valid_map_line(char *str);
 char			**convert_map(t_list *map, t_input *input);
 char			*fill_map_line(char *content, unsigned int len);
 mlx_texture_t	*open_texture(char *path);
 void			boundary_test(char **m_p, unsigned int height, unsigned int width);
-void			test_inputs(t_input *input);
-void			clean_input(t_input *input);
+void			test_inputs(t_check *check);
+
 
 #endif
