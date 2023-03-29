1cd004f2a63a7cbc08948f58c24decf4e663718c
greedymelon <74475072+greedymelon@users.noreply.github.com>
2023-03-29 10:59:38 +0200

    Update draw_main.c

--- src/draw_main.c
+++ src/draw_main.c
@@ -81,3 +81,26 @@ int	draw_cube(t_exe_info *info)
 	mlx_image_to_window(info->mlx, info->scene, 0, 0);
 	return (MLX_SUCCESS);
 }
+
+void draw_ray_scene(t_exe_info *img)
+{
+	t_wall_pos		w_pos;
+	t_print_info	info;
+	double			ray_angle;
+	int				i;
+
+	i = 0;
+	ray_angle = img->player.angle - (0.0174533 * (FOV / 2));
+	while (i < HEIGHT_WIDTH)
+	{
+		angle_normalizer(&ray_angle);
+		find_horiz_wall(img, &w_pos, ray_angle);
+		find_vert_wall(img, &w_pos, ray_angle);
+		set_print(img, &info, &w_pos);
+		draw_scene(img, &w_pos, i, img->player.angle - ray_angle);
+		if (img->pad.x < HEIGHT_WIDTH)
+			draw_ray(img, &info);
+		ray_angle += (0.0174533 * FOV) / HEIGHT_WIDTH;
+		i++;
+	}
+}
