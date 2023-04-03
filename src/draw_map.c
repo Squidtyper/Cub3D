c742caf6f59ffe105413c9f141d890cc333d5f56
greedymelon <74475072+greedymelon@users.noreply.github.com>
2023-04-03 19:48:06 +0200

    Update draw_map.c

--- src/draw_map.c
+++ src/draw_map.c
@@ -16,24 +16,24 @@ static void	st_draw_square(t_print_info *value)
 {
 	uint32_t	x_i;
 
-	while (value->start_y < value->end_y - 1)
+	while (value->start_point.y < value->end_point.y - 1)
 	{
-		x_i = value->start_x + 1;
-		while (x_i < value->end_x -1)
+		x_i = value->start_point.x + 1;
+		while (x_i < value->end_point.x -1)
 		{
-			mlx_put_pixel(value->img, x_i, value->start_y, value->color);
+			mlx_put_pixel(value->img, x_i, value->start_point.y, value->color);
 			x_i++;
 		}
-		value->start_y++;
+		value->start_point.y++;
 	}
 }
 
 static void	st_set_info(t_print_info *value, t_exe_info *info, int y, int x)
 {
-	value->start_y = y * info->blk_size + info->pad_y;
-	value->start_x = x * info->blk_size + info->pad_x;
-	value->end_y = (y + 1) * info->blk_size + info->pad_y;
-	value->end_x = (x + 1) * info->blk_size + info->pad_x;
+	value->start_point.y = y * info->size + info->pad.y;
+	value->start_point.x = x * info->size + info->pad.x;
+	value->end_point.y = (y + 1) * info->size + info->pad.y;
+	value->end_point.x = (x + 1) * info->size + info->pad.x;
 	value->img = info->background;
 }
 
@@ -43,7 +43,7 @@ void	draw_map(t_exe_info *info)
 	unsigned int	x;
 	t_print_info	value;
 
-	if (info->pad_x == HEIGHT_WIDTH)
+	if (info->pad.x == HEIGHT_WIDTH)
 		return ;
 	y = 0;
 	while (y < info->map_input->map_height)
@@ -51,7 +51,7 @@ void	draw_map(t_exe_info *info)
 		x = 0;
 		while (x < info->map_input->map_width)
 		{
-			if (info->map_input->map_points[y][x] == '1')
+			if (is_wall(info->map_input->map_points[y][x]))
 				value.color = 0xFFFFFFFF;
 			else
 				value.color = 0x000000FF;
